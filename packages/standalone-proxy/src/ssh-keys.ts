bbb44581bbabfb7e636d4df3b34733f5576666c6
Yshay Yaacobi <yshayy@gmail.com>
2023-03-06 12:33:04 +0200

    fix inline

--- packages/standalone-proxy/src/ssh-keys.ts
+++ packages/standalone-proxy/src/ssh-keys.ts
@@ -5,10 +5,10 @@ import ssh2 from "ssh2"
 export async function getSSHKeys({
   defaultKeyLocation,
 }: {defaultKeyLocation: string}){
-    const ssh_host_key_inline = process.env["SSH_HOST_KEY"]
+    const sshHostKeyInline = process.env["SSH_HOST_KEY"]
     let privateKeyContents = ""
-    if (ssh_host_key_inline) {
-      privateKeyContents = Buffer.from(ssh_host_key_inline, 'base64').toString('utf8')
+    if (sshHostKeyInline) {
+      privateKeyContents = Buffer.from(sshHostKeyInline, 'base64').toString('utf8')
     } else {
       try {
         privateKeyContents = await fs.readFile(path.resolve(".", process.env["SSH_HOST_KEY_PATH"] ?? defaultKeyLocation), {encoding: 'utf8'})
@@ -28,4 +28,5 @@ export async function getSSHKeys({
     } else {
       return {sshPrivateKey: privateKeyContents, sshPublicKey: pkey.getPublicSSH().toString("utf-8")}
     }
+    
   }
