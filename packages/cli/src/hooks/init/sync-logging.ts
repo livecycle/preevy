3c9e67cf867e175b07f7fef990a5b9854c287b29
Roy Razon <roy@livecycle.io>
2023-03-19 09:28:20 +0200

    sync logging

---
+++ packages/cli/src/hooks/init/sync-logging.ts
@@ -0,0 +1,12 @@
+import { Hook } from '@oclif/core'
+
+const hook: Hook<'init'> = async () => {
+  [process.stdout, process.stderr].forEach(stream => {
+    if (stream?.isTTY) {
+      // eslint-disable-next-line no-underscore-dangle
+      (stream as { _handle?: { setBlocking: (blocking: boolean) => void }})._handle?.setBlocking(true)
+    }
+  })
+}
+
+export default hook
