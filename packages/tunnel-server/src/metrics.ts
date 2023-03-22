bacec60a352ecf3cc7d4f71d27dde0f9da39f486
Yshay Yaacobi <yshayy@gmail.com>
2023-03-22 16:23:01 +0200

    added prom metrics and change logs a bit

---
+++ packages/tunnel-server/src/metrics.ts
@@ -0,0 +1,28 @@
+import fastify from 'fastify'
+import { Gauge , Counter,register } from 'prom-client';
+
+export const tunnelsGauge = new Gauge({
+  name: 'tunnels',
+  help: 'Current number of open tunnels',
+  labelNames: ['clientId'],
+})
+
+export const requestsCounter = new Counter({
+  name: 'requests',
+  help: 'Counter of incoming requests',
+  labelNames: ['clientId'],
+})
+
+register.setDefaultLabels({ serviceName: 'preevy-tunnel-server' })
+
+export function runMetricsServer(port: number){
+  const app = fastify();
+
+  app.get("/metrics", async (request, reply) => {
+    reply.header('Content-Type', register.contentType);
+		reply.send(await register.metrics())
+  });
+  return app.listen({
+    port,
+  })
+}
