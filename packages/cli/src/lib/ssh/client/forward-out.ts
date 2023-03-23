f02eb0b5a9280eb37d04d5dfd34aca9913b306d3
Roy Razon <roy@livecycle.io>
2023-03-23 18:33:59 +0200

    added error handlers in ssh forwardOut

---
+++ packages/cli/src/lib/ssh/client/forward-out.ts
@@ -0,0 +1,59 @@
+import net from 'net'
+import path from 'path'
+import ssh2 from 'ssh2'
+import { randomBytes } from 'crypto'
+import rimraf from 'rimraf'
+import { Logger } from '../../../log'
+
+export type ForwardOutStreamLocal = {
+  localSocket: string
+  close: () => void
+}
+
+export const forwardOutStreamLocal = (
+  ssh: ssh2.Client,
+  log: Logger,
+  lazySocketDir: () => string,
+) => async (
+  remoteSocket: string,
+) => new Promise<ForwardOutStreamLocal>((resolve, reject) => {
+  const socketPath = path.join(lazySocketDir(), `s_${randomBytes(16).toString('hex')}`)
+
+  const socketServer = net.createServer(socket => {
+    ssh.openssh_forwardOutStreamLocal(remoteSocket, (err, upstream) => {
+      if (err) {
+        log.error('openssh_forwardOutStreamLocal error', err)
+        socket.end()
+        socketServer.close()
+        reject(err)
+        return
+      }
+
+      upstream.on('error', (e: unknown) => log.error(`upstream error on socket ${socketPath}`, e))
+      socket.on('error', (e: unknown) => log.error(`socket error on socket ${socketPath}`, e))
+
+      upstream.pipe(socket).pipe(upstream)
+    })
+  })
+
+  const onConnectionClose = () => {
+    log.debug('client close, closing socketServer')
+    socketServer.close()
+  }
+
+  socketServer
+    .listen(socketPath, () => {
+      resolve({ localSocket: socketPath, close: () => socketServer.close() })
+    })
+    .on('error', (err: unknown) => {
+      log.error('socketServer error', err)
+      socketServer.close()
+    })
+    .on('close', async () => {
+      log.debug('socketServer closed')
+      ssh.removeListener('close', onConnectionClose)
+      await rimraf(socketPath)
+    })
+
+  ssh.on('close', onConnectionClose)
+})
