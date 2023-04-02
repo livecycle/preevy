00c93fb4921ec11fe2f6578de38b2074f751209d
Roy Razon <roy@livecycle.io>
2023-04-02 12:42:31 +0300

    full async trace

--- packages/cli/src/hooks/init/async-trace.ts
+++ packages/cli/src/hooks/init/async-trace.ts
@@ -4,24 +4,26 @@ import { Hook } from '@oclif/core'
 const hook: Hook<'init'> = async () => {
   const traces = new Map()
 
+  const originalCaptureStackTrace = global.Error.captureStackTrace
+
+  const captureStackTrace = (what: { stack: string }, where: (...args: unknown[]) => unknown) => {
+    originalCaptureStackTrace.call(global.Error, what, where)
+    const trace = traces.get(ah.executionAsyncId())
+    if (trace) what.stack += trace
+  }
+
   ah.createHook({
     init(id: number) {
       const trace = {}
       Error.captureStackTrace(trace)
-      traces.set(id, (trace as { stack: string }).stack.replace(/(^.+$\n){4}/m, '\n'))
+      traces.set(id, (trace as { stack: string }).stack) // .replace(/(^.+$\n){4}/m, '\n'))
     },
     destroy(id) {
       traces.delete(id)
     },
   }).enable()
 
-  const originalCaptureStackTrace = global.Error.captureStackTrace
-
-  global.Error.captureStackTrace = (what: { stack: string }, where: (...args: unknown[]) => unknown) => {
-    originalCaptureStackTrace.call(global.Error, what, where)
-    const trace = traces.get(ah.executionAsyncId())
-    if (trace) what.stack += trace
-  }
+  global.Error.captureStackTrace = captureStackTrace
 }
 
 export default hook
