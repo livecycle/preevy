e0dd65bdbee630f536e5f920faefbed844b4d701
Yshayy <yshayy@gmail.com>
2023-05-04 13:15:18 +0300

    Extract tunnel server from workspace config (#59)
    * remove common dep from tunnel server.
    remove tunnel server from workspace config.
    update gh file
    
    * revert
    
    * updated lock file

---
+++ tunnel-server/src/utils.ts
@@ -0,0 +1,12 @@
+export const requiredEnv = (key: string): string => {
+  const result = process.env[key]
+  if (!result) {
+    throw new Error(`required env var ${key} not set`)
+  }
+  return result
+}
+
+export const numberFromEnv = (key: string) => {
+  const s = process.env[key]
+  return s === undefined ? undefined : Number(s)
+}
