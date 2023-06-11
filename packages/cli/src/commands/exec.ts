881611137d87d9aafb95b092ff602754d3ccfd4e
Roy Razon <roy@livecycle.io>
2023-06-11 15:11:23 +0300

    fixes
    - CR fix
    - add test
    - add GH action for running test

--- packages/cli/src/commands/exec.ts
+++ packages/cli/src/commands/exec.ts
@@ -1,10 +1,11 @@
 import { Args } from '@oclif/core'
-import { commands, sshKeysStore } from '@preevy/core'
+import { commands } from '@preevy/core'
 import DriverCommand from '../driver-command'
 
 // eslint-disable-next-line no-use-before-define
-export default class Ssh extends DriverCommand<typeof Ssh> {
-  static description = 'SSH into a preview environment machine'
+export default class Exec extends DriverCommand<typeof Exec> {
+  static description = 'Execute a command or start an interactive shell inside an environment'
+  static aliases = ['ssh']
 
   static hidden = true
   static strict = false
@@ -19,20 +20,14 @@ export default class Ssh extends DriverCommand<typeof Ssh> {
   static enableJsonFlag = false
 
   async run(): Promise<unknown> {
-    const { args, raw } = await this.parse(Ssh)
+    const { args, raw } = await this.parse(Exec)
     const driver = await this.driver()
-    const sshKey = await sshKeysStore(this.store).getKey(await driver.getKeyPairAlias())
-    if (!sshKey) {
-      this.error('SSH key not found for connecting to machine')
-    }
 
     const restArgs = raw.filter(arg => arg.type === 'arg').slice(1).map(arg => arg.input)
 
-    const { code } = await commands.ssh({
+    const { code } = await commands.exec({
       envId: args.envId,
-      dataDir: this.config.dataDir,
       args: restArgs,
-      sshKeyPair: sshKey,
       machineDriver: driver,
       log: this.logger,
     })
--- packages/cli/src/commands/ls.ts
+++ packages/cli/src/commands/ls.ts
@@ -19,7 +19,12 @@ export default class Ls extends DriverCommand<typeof Ls> {
   async run(): Promise<unknown> {
     const { flags } = await this.parse(Ls)
     const driver = await this.driver()
-    const machines = await asyncToArray(asyncMap(x => ({ ...x, state: ('error' in x) ? x.error : 'OK' }), await commands.ls({ machineDriver: driver, log: this.logger })))
+    const machines = await asyncToArray(
+      asyncMap(
+        x => ({ ...x, state: ('error' in x) ? x.error : 'OK' }),
+        await commands.ls({ machineDriver: driver, log: this.logger }),
+      ),
+    )
 
     if (flags.json) {
       return machines
@@ -30,7 +35,7 @@ export default class Ls extends DriverCommand<typeof Ls> {
       {
         envId: { header: 'Env' },
         providerId: { header: 'Driver ID' },
-        publicIPAddress: { header: 'IP address' },
+        locationDescription: { header: 'Location' },
         version: { header: 'Version', extended: true },
         state: { header: 'State' },
       },
