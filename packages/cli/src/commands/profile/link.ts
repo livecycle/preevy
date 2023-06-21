fb11b8dbbb952c7e0a7967bff12ab5d528cd197a
Assaf Krintza <assafk88@gmail.com>
2023-06-21 09:35:14 +0300

    change flag name

--- packages/cli/src/commands/profile/link.ts
+++ packages/cli/src/commands/profile/link.ts
@@ -6,13 +6,13 @@ import ProfileCommand from '../../profile-command'
 // eslint-disable-next-line no-use-before-define
 export default class Link extends ProfileCommand<typeof Link> {
   static flags = {
-    lcUrl: Flags.string({ required: false, default: 'http://localhost:3000/api', description: "The Livecycle API URL'" }),
+    'lc-url': Flags.string({ required: false, default: 'http://localhost:3000/api', description: "The Livecycle API URL'" }),
   }
 
   static description = "Link the profile to the logged in user's organization"
 
   async run(): Promise<unknown> {
-    await link(this.store, this.config.dataDir, this.flags.lcUrl, this.logger, async (orgs: Org[]) => {
+    await link(this.store, this.config.dataDir, this.flags['lc-url'], this.logger, async (orgs: Org[]) => {
       const { org } = await inquirer.prompt<{org: string}>({ type: 'list', name: 'org', message: 'Choose the organization to link the profile to', choices: orgs.map(o => o.name) })
       return orgs.find(o => o.name === org) as Org
     })
