c5fb0ee0252e11f6eb53b165123e581bc4d33a2c
Roy Razon <roy@livecycle.io>
2023-07-10 10:54:41 +0300

    converted from callbacks to events

--- tunnel-server/index.ts
+++ tunnel-server/index.ts
@@ -1,4 +1,4 @@
-import { promisify } from 'util'
+import { inspect, promisify } from 'util'
 import { app as createApp } from './src/app'
 import { inMemoryPreviewEnvStore } from './src/preview-env'
 import { sshServer as createSshServer } from './src/ssh-server'
@@ -13,7 +13,6 @@ import { runMetricsServer } from './src/metrics'
 import { numberFromEnv, requiredEnv } from './src/env'
 import { replaceHostname } from './src/url'
 import { createPublicKey } from 'crypto'
-import lodash from 'lodash'
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
@@ -57,39 +56,47 @@ const sshServer = createSshServer({
   log: sshLogger,
   sshPrivateKey,
   socketDir: '/tmp', // TODO
-  validateForwardRequest: async ({ client: { clientId }, parsedRequest: { path: remotePath  } }) => {
-    const key = tunnelName(clientId, remotePath)
-    if (await envStore.has(key)) {
-      throw new Error(`duplicate path: ${key}`)
-    }
-  },
-  onForwardSocketCreated: async ({ client: { clientId, publicKey }, parsedRequest: { path: remotePath, access }, localSocketPath }) => {
-    const key = tunnelName(clientId, remotePath);
-    sshLogger.debug('creating tunnel %s for localSocket %s', key, localSocketPath)
-    await envStore.set(key, {
-      target: localSocketPath,
-      clientId,
-      publicKey: createPublicKey(publicKey.getPublicPEM()),
-      access,
-    })
-    tunnelsGauge.inc({clientId})
-  },
-  onForwardSocketDestroyed: async ({ client: { clientId }, parsedRequest: { path: remotePath } }) => {
-    const key = tunnelName(clientId, remotePath);
-    sshLogger.debug('deleting tunnel %s', key)
-    await envStore.delete(key)
-    tunnelsGauge.dec({clientId})
-  },
-  onHello: async ({ clientId }, tunnels) => {
-    return JSON.stringify({
-      clientId,
-      // TODO: backwards compat, remove when we drop support for CLI v0.0.35
-      baseUrl: { hostname: BASE_URL.hostname, port: BASE_URL.port, protocol: BASE_URL.protocol },
-      rootUrl: BASE_URL.toString(),
-      tunnels: lodash.mapValues(tunnels, ({ path: remotePath }) => tunnelUrl(BASE_URL, clientId, remotePath)),
-    }) + '\r\n'
-  },
 })
+  .on('client', client => {
+    const { clientId, publicKey } = client
+    const tunnels = new Map<string, string>()
+    client
+      .on('forward', async (requestId, { path: remotePath, access }, accept, reject) => {
+        const key = tunnelName(clientId, remotePath)
+        if (await envStore.has(key)) {
+          reject(new Error(`duplicate path: ${key}`))
+          return
+        }
+        const forward = await accept()
+        sshLogger.debug('creating tunnel %s for localSocket %s', key, forward.localSocketPath)
+        await envStore.set(key, {
+          target: forward.localSocketPath,
+          clientId,
+          publicKey: createPublicKey(publicKey.getPublicPEM()),
+          access,
+        })
+        tunnels.set(requestId, tunnelUrl(BASE_URL, clientId, remotePath))
+        tunnelsGauge.inc({clientId})
+
+        forward.on('close', () => {
+          sshLogger.debug('deleting tunnel %s', key)
+          tunnels.delete(requestId)
+          void envStore.delete(key)
+          tunnelsGauge.dec({clientId})
+        })
+      })
+      .on('error', err => { sshLogger.warn('client error %j: %j', clientId, inspect(err)) })
+      .on('hello', channel => {
+        channel.stdout.write(JSON.stringify({
+          clientId,
+          // TODO: backwards compat, remove when we drop support for CLI v0.0.35
+          baseUrl: { hostname: BASE_URL.hostname, port: BASE_URL.port, protocol: BASE_URL.protocol },
+          rootUrl: BASE_URL.toString(),
+          tunnels: Object.fromEntries(tunnels.entries()),
+        }) + '\r\n')
+        channel.exit(0)
+      })
+  })
   .listen(SSH_PORT, LISTEN_HOST, () => {
     app.log.debug('ssh server listening on port %j', SSH_PORT)
   })
--- tunnel-server/src/ssh-server.ts
+++ tunnel-server/src/ssh-server.ts
@@ -5,6 +5,8 @@ import path from 'path'
 import ssh2, { ParsedKey, SocketBindInfo } from 'ssh2'
 import { inspect } from 'util'
 import { ForwardRequest, parseForwardRequest } from './forward-request'
+import EventEmitter from 'node:events'
+import { Writable } from 'node:stream'
 
 const idFromPublicSsh = (key: Buffer) =>
   crypto.createHash('sha1').update(key).digest('base64url').replace(/[_-]/g, '').slice(0, 8).toLowerCase()
@@ -19,204 +21,249 @@ const parseForwardRequestFromSocketBindInfo = (
   }
 }
 
-export type RemoteClient = {
+export interface ClientForward extends EventEmitter {
+  localSocketPath: string
+  on: (event: 'close', listener: () => void) => this
+}
+
+export interface SshClient extends EventEmitter {
   clientId: string
   publicKey: ParsedKey
+  on: (
+    (
+      event: 'forward',
+      listener: (
+        requestId: string,
+        request: ForwardRequest,
+        accept: () => Promise<ClientForward>,
+        reject: (reason: Error) => void,
+      ) => void
+    ) => this
+  ) & (
+    (
+      event: 'hello',
+      listener: (
+        channel: { stdout: Writable; exit: (status?: number) => void },
+      ) => void
+    ) => this
+  ) & (
+    (
+      event: 'error',
+      listener: (err: Error) => void,
+    ) => this
+  )
 }
 
-export type ForwardRequestWithContext = {
-  request: string
-  parsedRequest: ForwardRequest
-  client: RemoteClient
+export interface SshServer extends EventEmitter {
+  close: ssh2.Server['close']
+  listen: ssh2.Server['listen']
+  on: (
+    (
+      event: 'client',
+      listener: (client: SshClient) => void,
+    ) => this
+  ) & (
+    (
+      event: 'error',
+      listener: (err: Error) => void,
+    ) => this
+  )
 }
 
-export type ForwardSocketEvent = ForwardRequestWithContext & {
-  localSocketPath: string
-}
-
-export const sshServer = ({
-  log,
-  sshPrivateKey,
-  socketDir,
-  onForwardSocketCreated: onPipeCreated,
-  onForwardSocketDestroyed: onPipeDestroyed,
-  validateForwardRequest,
-  onHello,
-}: {
-  log: FastifyBaseLogger
-  sshPrivateKey: string
-  socketDir: string
-  validateForwardRequest?: (request: ForwardRequestWithContext) => Promise<void>
-  onForwardSocketCreated?: (event: ForwardSocketEvent) => void
-  onForwardSocketDestroyed?: (event: ForwardSocketEvent) => void
-  onHello: (client: RemoteClient, activeTunnelRequests: Record<string, ForwardRequest>) => Promise<string>
-}) => new ssh2.Server(
+export const sshServer = (
   {
-    debug: x => log.debug(x),
-    // keepaliveInterval: 1000,
-    // keepaliveCountMax: 5,
-    hostKeys: [sshPrivateKey],
-  },
-  (client) => {
-    let remoteClient: RemoteClient
-    const tunnels = new Map<string, ForwardRequest>()
+    log,
+    sshPrivateKey,
+    socketDir,
+  }: {
+    log: FastifyBaseLogger
+    sshPrivateKey: string
+    socketDir: string
+  }
+): SshServer => {
+  const serverEmitter = new EventEmitter() as Omit<SshServer, 'close' | 'listen'>
+  const server = new ssh2.Server(
+    {
+      debug: x => log.debug(x),
+      // keepaliveInterval: 1000,
+      // keepaliveCountMax: 5,
+      hostKeys: [sshPrivateKey],
+    },
+    client => {
+      let preevySshClient: SshClient
+      const tunnels = new Map<string, { close: () => void }>()
 
-    client
-      .on('error', (err) => log.error(`client error: %j`, inspect(err)))
-      .on('authentication', (ctx) => {
-        log.debug('authentication: %j', ctx)
-        if (ctx.method !== 'publickey') {
-          ctx.reject(['publickey'])
-          return
-        }
+      client
+        .on('error', err => log.error(`client error: %j`, inspect(err)))
+        .on('authentication', (ctx) => {
+          log.debug('authentication: %j', ctx)
+          if (ctx.method !== 'publickey') {
+            ctx.reject(['publickey'])
+            return
+          }
 
-        const keyOrError = ssh2.utils.parseKey(ctx.key.data)
-        if (!('getPublicSSH' in keyOrError)) {
-          log.error('error parsing key: %j', keyOrError)
-          ctx.reject()
-          return
-        }
+          const keyOrError = ssh2.utils.parseKey(ctx.key.data)
+          if (!('getPublicSSH' in keyOrError)) {
+            log.error('error parsing key: %j', keyOrError)
+            ctx.reject()
+            return
+          }
 
-        // calling "accept" when no signature specified does not result in authenticated state
-        // see: https://github.com/mscdex/ssh2/issues/561#issuecomment-303263753
-        if (ctx.signature && !keyOrError.verify(ctx.blob as Buffer, ctx.signature, ctx.key.algo)) {
-          log.error('error verifying key: %j', keyOrError)
-          ctx.reject(['publickey'])
-          return
-        }
+          // calling "accept" when no signature specified does not result in authenticated state
+          // see: https://github.com/mscdex/ssh2/issues/561#issuecomment-303263753
+          if (ctx.signature && !keyOrError.verify(ctx.blob as Buffer, ctx.signature, ctx.key.algo)) {
+            log.error('error verifying key: %j', keyOrError)
+            ctx.reject(['publickey'])
+            return
+          }
 
-        remoteClient = { publicKey: keyOrError, clientId: idFromPublicSsh(keyOrError.getPublicSSH()) }
-        log.debug('accepting clientId %j', remoteClient.clientId)
-        ctx.accept()
-      })
-      .on('request', async (accept, reject, name, info) => {
-        log.debug('request %j', { accept, reject, name, info })
-        if (!client.authenticated) {
-          log.error('not authenticated, rejecting')
-          reject?.()
-          return
-        }
+          preevySshClient = Object.assign(new EventEmitter(), {
+            publicKey: keyOrError,
+            clientId: idFromPublicSsh(keyOrError.getPublicSSH()),
+          })
+          log.debug('accepting clientId %j', preevySshClient.clientId)
+          ctx.accept()
+          serverEmitter.emit('client', preevySshClient)
+        })
+        .on('request', async (accept, reject, name, info) => {
+          log.debug('request %j', { accept, reject, name, info })
+          if (!client.authenticated) {
+            log.error('not authenticated, rejecting')
+            reject?.()
+            return
+          }
 
-        if ((name as string) == 'cancel-streamlocal-forward@openssh.com') {
+          if ((name as string) == 'cancel-streamlocal-forward@openssh.com') {
+            const res = parseForwardRequestFromSocketBindInfo(info as unknown as SocketBindInfo)
+            if('error' in res) {
+              log.error('cancel-streamlocal-forward@openssh.com: invalid socket path %j: %j', res.request, res.error)
+              reject?.()
+              return
+            }
+            const deleted = tunnels.get(res.request)
+            if (!deleted) {
+              log.error('cancel-streamlocal-forward@openssh.com: request %j not found, rejecting', res.request)
+              reject?.()
+              return
+            }
+            deleted.close()
+            accept?.()
+            return
+          }
+
+          if ((name as string) !== 'streamlocal-forward@openssh.com') {
+            log.error('invalid request %j', { name, info })
+            reject?.()
+            return
+          }
+
           const res = parseForwardRequestFromSocketBindInfo(info as unknown as SocketBindInfo)
           if('error' in res) {
-            log.error('cancel-streamlocal-forward@openssh.com: invalid socket path %j: %j', res.request, res.error)
+            log.error('streamlocal-forward@openssh.com: rejecting %j, error parsing: %j', res.request, inspect(res.error))
             reject?.()
             return
           }
-          if (!tunnels.delete(res.request)) {
-            log.error('cancel-streamlocal-forward@openssh.com: request %j not found, rejecting', res.request)
+
+          const { request, parsed } = res
+
+          if (tunnels.has(request)) {
+            log.error('streamlocal-forward@openssh.com: rejecting %j, duplicate socket request', res.request)
             reject?.()
+            return
           }
-          accept?.()
-          return
-        }
 
-        if ((name as string) !== 'streamlocal-forward@openssh.com') {
-          log.error('invalid request %j', { name, info })
-          reject?.()
-          return
-        }
-
-        const res = parseForwardRequestFromSocketBindInfo(info as unknown as SocketBindInfo)
-        if('error' in res) {
-          log.error('streamlocal-forward@openssh.com: rejecting %j, error parsing: %j', res.request, inspect(res.error))
-          reject?.()
-          return
-        }
-
-        const { request, parsed } = res
-
-        if (tunnels.has(request)) {
-          log.error('streamlocal-forward@openssh.com: rejecting %j, duplicate socket request', res.request)
-          reject?.()
-          return
-        }
-
-        try {
-          await validateForwardRequest?.({ client: remoteClient, request, parsedRequest: parsed })
-        } catch (validationError) {
-          log.error(
-            'streamlocal-forward@openssh.com: rejecting %j, validateForwardRequest returned error: %j',
-            res.request,
-            inspect(validationError),
-          )
-          reject?.()
-          return
-        }
-
-        const socketServer = net.createServer((socket) => {
-          log.debug('socketServer connected %j', socket)
-          client.openssh_forwardOutStreamLocal(
+          preevySshClient.emit(
+            'forward',
             request,
-            (err, upstream) => {
-              if (err) {
-                log.error('error forwarding request %j: %s', request, inspect(err))
-                socket.end()
-                socketServer.close((err) => {
-                  log.error('error closing socket server for request %j: %j', request, inspect(err))
+            parsed,
+            () => new Promise<ClientForward>((resolve, reject) => {
+              const socketServer = net.createServer((socket) => {
+                log.debug('socketServer connected %j', socket)
+                client.openssh_forwardOutStreamLocal(
+                  request,
+                  (err, upstream) => {
+                    if (err) {
+                      log.error('error forwarding request %j: %s', request, inspect(err))
+                      socket.end()
+                      socketServer.close((err) => {
+                        log.error('error closing socket server for request %j: %j', request, inspect(err))
+                      })
+                      return
+                    }
+                    upstream.pipe(socket).pipe(upstream)
+                  }
+                )
+              })
+
+              const socketPath = path.join(socketDir, `s_${preevySshClient.clientId}_${randomBytes(16).toString('hex')}`)
+
+              const closeSocketServer = () => socketServer.close()
+
+              socketServer
+                .listen(socketPath, () => {
+                  log.debug('streamlocal-forward@openssh.com: calling accept: %j', accept)
+                  accept?.()
+                  tunnels.set(request, socketServer)
+                  resolve(Object.assign(socketServer, {
+                    localSocketPath: socketPath,
+                  }))
                 })
-                return
-              }
-              upstream.pipe(socket).pipe(upstream)
+                .on('error', (err: unknown) => {
+                  log.error('socketServer error: %j', err)
+                  socketServer.close()
+                  reject(err)
+                })
+                .on('close', () => {
+                  log.debug('socketServer close: %j', socketPath)
+                  tunnels.delete(request)
+                  client.removeListener('close', closeSocketServer)
+                })
+
+              client.once('close', closeSocketServer)
+            }),
+            (reason: Error) => {
+              log.error('streamlocal-forward@openssh.com: rejecting %j, reason: %j', res.request, inspect(reason))
+              reject?.()
             }
           )
+
         })
+        .on('error', err => { preevySshClient.emit('error', err) })
+        .on('session', accept => {
+          log.debug('session')
+          const session = accept()
 
-        const socketPath = path.join(socketDir, `s_${remoteClient.clientId}_${randomBytes(16).toString('hex')}`)
+          session.on('exec', async (accept, reject, info) => {
+            log.debug('exec %j', info)
+            if (info.command !== 'hello') {
+              log.error('invalid exec command %j', info.command)
+              reject()
+              return
+            }
 
-        const closeSocketServer = () => socketServer.close()
-
-        socketServer
-          .listen(socketPath, () => {
-            log.debug('streamlocal-forward@openssh.com: calling accept: %j', accept)
-            accept?.()
-            tunnels.set(request, parsed)
-            onPipeCreated?.({
-              request,
-              parsedRequest: parsed,
-              client: remoteClient,
-              localSocketPath: socketPath,
+            const channel = accept()
+            preevySshClient.emit('hello', {
+              stdout: channel.stdout,
+              exit: (status: number) => {
+                channel.stdout.exit(status)
+                if (tunnels.size === 0) {
+                  channel.close()
+                }
+              },
             })
+            // channel.stdout.on('close', () => {
+            //   log.debug('hello close, tunnels: %d', tunnels.size)
+            //   if (tunnels.size === 0) {
+            //     channel.close()
+            //   }
+            // })
           })
-          .on('error', (err: unknown) => {
-            log.error('socketServer error: %j', err)
-            socketServer.close()
-          })
-          .on('close', () => {
-            log.debug('socketServer close: %j', socketPath)
-            tunnels.delete(request)
-            onPipeDestroyed?.({
-              request,
-              parsedRequest: parsed,
-              client: remoteClient,
-              localSocketPath: socketPath,
-            })
-            client.removeListener('close', closeSocketServer)
-          })
-
-        client.once('close', closeSocketServer)
-      })
-      .on('session', (accept) => {
-        log.debug('session')
-        const session = accept()
-
-        session.on('exec', async (accept, reject, info) => {
-          log.debug('exec %j', info)
-          if (info.command !== 'hello') {
-            log.error('invalid exec command %j', info.command)
-            reject()
-            return
-          }
-          const channel = accept()
-          const tunnelsObj = Object.fromEntries([...tunnels.entries()])
-          log.info({ clientId: remoteClient.clientId, tunnels: tunnelsObj }, 'client hello')
-          channel.stdout.write(await onHello({ ...remoteClient }, tunnelsObj))
-          channel.stdout.exit(0)
-          if (tunnels.size === 0) {
-            channel.close()
-          }
         })
-      })
-  }
-)
+    }
+  )
+
+  return Object.assign(serverEmitter, {
+    close: server.close.bind(server),
+    listen: server.listen.bind(server),
+  })
+}
