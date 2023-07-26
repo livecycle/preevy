cd16b0c0d3834042f5fe234210781e53e6857c23
Yshay Yaacobi <yshay@livecycle.io>
2023-07-26 18:30:51 +0300

    setup GH release, update docker image, use depot in CI

--- packages/core/src/compose-tunnel-agent-client.ts
+++ packages/core/src/compose-tunnel-agent-client.ts
@@ -1,7 +1,9 @@
 import path from 'path'
 import fetch from 'node-fetch'
 import retry from 'p-retry'
-import { mapValues } from 'lodash'
+import { mapValues, memoize } from 'lodash'
+import { copyFileSync, existsSync, mkdirSync, mkdtempSync, readdirSync, statSync } from 'fs'
+import { tmpdir } from 'os'
 import { ComposeModel, ComposeService } from './compose/model'
 import { TunnelOpts } from './ssh/url'
 import { Tunnel } from './tunneling'
@@ -9,23 +11,45 @@ import { withBasicAuthCredentials } from './url'
 
 export const COMPOSE_TUNNEL_AGENT_SERVICE_NAME = 'preevy_proxy'
 export const COMPOSE_TUNNEL_AGENT_SERVICE_PORT = 3000
-const COMPOSE_TUNNEL_AGENT_DIR = path.join(__dirname, '../../compose-tunnel-agent')
-
-const baseDockerProxyService: ComposeService = {
-  build: {
-    context: COMPOSE_TUNNEL_AGENT_DIR,
-  },
-  labels: {
-    'preevy.access': 'private',
-  },
+const COMPOSE_TUNNEL_AGENT_DIR = path.join(path.dirname(require.resolve('@preevy/compose-tunnel-agent')), '..')
+declare let process : {
+  pkg?: {}
 }
 
-export const minimalModelWithDockerProxyService = (name: string): ComposeModel => ({
-  name,
-  services: {
-    [COMPOSE_TUNNEL_AGENT_SERVICE_NAME]: baseDockerProxyService,
-  },
+const pkgComposeTunnelAgentDirFromSnapshot = memoize(() => {
+  // can't use fs.cpSync because it's not patched by pkg (https://github.com/vercel/pkg/blob/bb042694e4289a1cbc530d2938babe35ccc84a93/prelude/bootstrap.js#L600)
+  const copyDirRecursive = (sourceDir: string, targetDir:string) => {
+    if (!existsSync(targetDir)) {
+      mkdirSync(targetDir)
+    }
+    const files = readdirSync(sourceDir)
+    for (const file of files) {
+      const sourcePath = path.join(sourceDir, file)
+      const targetPath = path.join(targetDir, file)
+      const stat = statSync(sourcePath)
+      if (stat.isDirectory()) {
+        copyDirRecursive(sourcePath, targetPath)
+      } else {
+        copyFileSync(sourcePath, targetPath)
+      }
+    }
+  }
+  const dest = mkdtempSync(path.join(tmpdir(), 'compose-tunnel-agent'))
+  copyDirRecursive(path.join(__dirname, '../../compose-tunnel-agent'), dest)
+  return dest
 })
+
+const baseDockerProxyService = () => {
+  const contextDir = process?.pkg ? pkgComposeTunnelAgentDirFromSnapshot() : COMPOSE_TUNNEL_AGENT_DIR
+  return {
+    build: {
+      context: contextDir,
+    },
+    labels: {
+      'preevy.access': 'private',
+    },
+  } as ComposeService
+}
 
 export const addBaseComposeTunnelAgentService = (
   model: ComposeModel,
@@ -33,7 +57,7 @@ export const addBaseComposeTunnelAgentService = (
   ...model,
   services: {
     ...model.services,
-    [COMPOSE_TUNNEL_AGENT_SERVICE_NAME]: baseDockerProxyService,
+    [COMPOSE_TUNNEL_AGENT_SERVICE_NAME]: baseDockerProxyService(),
   },
 })
 
@@ -52,7 +76,7 @@ export const addComposeTunnelAgentService = (
   services: {
     ...model.services,
     [COMPOSE_TUNNEL_AGENT_SERVICE_NAME]: {
-      ...baseDockerProxyService,
+      ...baseDockerProxyService(),
       restart: 'always',
       networks: Object.keys(model.networks || {}),
       ports: [
