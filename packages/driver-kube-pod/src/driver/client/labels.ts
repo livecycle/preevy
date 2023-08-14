6076bb72a22ced76ecd98ba0f1c6d9ff2c1662a5
Roy Razon <roy@livecycle.io>
2023-08-14 19:26:56 +0300

    kube driver: fix sanitation
    and add tests

--- packages/driver-kube-pod/src/driver/client/labels.ts
+++ packages/driver-kube-pod/src/driver/client/labels.ts
@@ -3,10 +3,12 @@ import { truncateWithHash } from '@preevy/core'
 
 const MAX_LABEL_LENGTH = 63
 
+// https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/#syntax-and-character-set
 export const sanitizeLabel = (s: string) => truncateWithHash(
   s
     .replace(/[^a-zA-Z0-9_.-]/g, '-')
-    .replace(/^[^a-zA-Z0-9]/, firstChar => `a${firstChar}`), // prepend alpha char if first char is not alpha
+    .replace(/^[^a-zA-Z0-9]/, firstChar => `a${firstChar}`) // prefix with alphanumeric if first char is not alphanumeric
+    .replace(/[^a-zA-Z0-9]$/, lastChar => `${lastChar}z`), // suffix with alphanumeric if last char is not alphanumeric
   MAX_LABEL_LENGTH,
 )
 
