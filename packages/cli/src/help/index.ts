b68b94ac886dba6ed9f284f4eacc16807efc6c6c
Abhijit Pachpande <abhijit@Abhijits-MacBook-Pro.local>
2023-08-19 18:46:17 +0530

    Cleaned code for custom help class

--- packages/cli/src/help/index.ts
+++ packages/cli/src/help/index.ts
@@ -1,38 +1,24 @@
 import { Help } from '@oclif/core'
 import { BaseCommand } from '@preevy/cli-common'
+import os from 'os'
 
-export default class MyHelpClass extends Help {
+export default class CustomHelpClass extends Help {
   protected async showRootHelp(): Promise<void> {
     await super.showRootHelp()
     this.log(this.formatFlag())
   }
 
   protected formatFlag():string {
-    type Flag = {
-      name: string
-      description: string
-    };
+ 
+    const flags = Object.entries(BaseCommand.baseFlags)
+      .filter(([_name, flag]) => flag.helpGroup === 'GLOBAL')
 
-    const flags: Flag[] = []
-
-    for (const flagName of Object.keys(BaseCommand.baseFlags)) {
-      if (BaseCommand.baseFlags.hasOwnProperty.call(BaseCommand.baseFlags, flagName)) {
-        const flagKey = flagName as keyof typeof BaseCommand.baseFlags
-        const flag = BaseCommand.baseFlags[flagKey]
-        if (flag.helpGroup === 'GLOBAL') {
-          const flagHelp: Flag = {
-            name: flagName,
-            description: flag.summary ?? '',
-          }
-          flags.push(flagHelp)
-        }
-      }
-    }
+      .map(([name, flag]) => ({ name, description: flag.summary ?? '' }))
     const body = this.renderList(flags.map(c => [
       `--${c.name}`,
       c.description,
     ]), {
-      spacer: '\n',
+      spacer: os.EOL,
       stripAnsi: this.opts.stripAnsi,
       indentation: 2,
     })
