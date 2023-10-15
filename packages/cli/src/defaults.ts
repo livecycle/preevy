c79a72bc043ae89358505f31f628ff1a0f0ddf8a
eliyabar <flashxyz@gmail.com>
2023-10-15 17:08:20 +0300

    fix auth url

--- packages/cli/src/defaults.ts
+++ packages/cli/src/defaults.ts
@@ -1,3 +1,3 @@
 export const LC_CLIENT_ID = 'BHXcVtapfKPEpZtYO3AJ2Livmz6j7xK0'
 export const LC_API_URL = 'https://app.livecycle.run'
-export const LC_AUTH_URL = 'https://livecycle.us.auth0.com'
+export const LC_AUTH_URL = 'https://auth.livecycle.dev'
