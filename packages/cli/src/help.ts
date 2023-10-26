3acda065284512b7f6841545a45988b481f05054
Roy Razon <roy@livecycle.io>
2023-10-26 13:49:07 +0300

    always show global flags
    fixes #34

--- packages/cli/src/help.ts
+++ packages/cli/src/help.ts
@@ -1,11 +1,46 @@
-import { Help as HelpBase } from '@oclif/core'
-import { text } from '@preevy/cli-common'
+import { CommandHelp as BaseCommandHelp, Help as OclifHelp } from '@oclif/core'
+import { HelpOptions, Config, Topic } from '@oclif/core/lib/interfaces'
+import { BaseCommand, text } from '@preevy/cli-common'
 
-export default class Help extends HelpBase {
+class GlobalFlagsHelp extends BaseCommandHelp {
+  constructor(config: Config, opts: HelpOptions) {
+    super(BaseCommand, config, opts)
+  }
+
+  globalFlags() {
+    const flags = Object.entries(BaseCommand.baseFlags).map(([name, value]) => ({ ...value, name }))
+    return this.flags(flags)
+  }
+}
+
+class CommandHelp extends BaseCommandHelp {
+  constructor(...args: ConstructorParameters<typeof BaseCommandHelp>) {
+    super(...args)
+  }
+}
+
+export default class Help extends OclifHelp {
+  constructor(...args: ConstructorParameters<typeof OclifHelp>) {
+    super(...args)
+    this.CommandHelpClass = CommandHelp
+  }
+
+  protected formatGlobalFlags(): string {
+    return this.section('GLOBAL FLAGS', new GlobalFlagsHelp(this.config, this.opts).globalFlags())
+  }
+
   override async showRootHelp(): Promise<void> {
     if (!this.opts.stripAnsi) {
       this.log(text.logo)
     }
-    return await super.showRootHelp()
+    await super.showRootHelp()
+    this.log(this.formatGlobalFlags())
+    this.log('')
+  }
+
+  override async showTopicHelp(topic: Topic): Promise<void> {
+    await super.showTopicHelp(topic)
+    this.log(this.formatGlobalFlags())
+    this.log('')
   }
 }
