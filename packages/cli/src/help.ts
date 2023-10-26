439cc537c1ed63f50dc2080a79531384efccd2eb
Roy Razon <roy@livecycle.io>
2023-10-26 11:01:37 +0300

    fix build

--- packages/cli/src/help.ts
+++ packages/cli/src/help.ts
@@ -1,10 +1,10 @@
 import { Help as HelpBase } from '@oclif/core'
-import { logo } from '@preevy/cli-common/src/lib/text'
+import { text } from '@preevy/cli-common'
 
 export default class Help extends HelpBase {
   override async showRootHelp(): Promise<void> {
     if (!this.opts.stripAnsi) {
-      this.log(logo)
+      this.log(text.logo)
     }
     return await super.showRootHelp()
   }
