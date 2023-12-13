d90ca1bfb2c5b08377710a91334c248dfb64272d
Roy Razon <roy@livecycle.io>
2023-12-13 10:32:04 +0200

    wip

---
+++ packages/core/jest.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  testMatch: ['!dist/', '**/*.test.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+}
