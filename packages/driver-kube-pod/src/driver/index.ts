d00063daaccf4700cf4b6f40960ef2b6615911f1
Roy Razon <roy@livecycle.io>
2024-03-25 15:08:24 +0200

    upgrade typescript

--- packages/driver-kube-pod/src/driver/index.ts
+++ packages/driver-kube-pod/src/driver/index.ts
@@ -1,6 +1,7 @@
 import { flags, factory } from './driver.js'
 import { flags as machineCreationFlags, factory as machineCreationFactory } from './creation-driver.js'
 import { inquireFlags } from './questions.js'
+import type {} from '@preevy/core' // https://github.com/microsoft/TypeScript/issues/47663#issuecomment-1270716220
 
 export type { StatefulSetMachine, DeploymentMachine, ResourceType } from './common.js'
 export type { MachineCreationFlagTypes } from './creation-driver.js'
